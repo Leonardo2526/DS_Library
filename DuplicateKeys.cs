1a8f734b762cafc8971c10709c51c98beb97faaa
Danil <58917020+Leonardo2526@users.noreply.github.com>
2021-02-21 19:05:17 +0300

    Add files via upload

---
+++ DuplicateKeys.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+public class Example
+{
+    public static void Main()
+    {
+        string[] Sarray = { "First", "Second", "Third", "First", "Third" };
+        string[] output = new string[Sarray.Length];
+
+        //Output if array contain repeated elements
+        if (IFDuplicateKeys(Sarray) == true)
+        {
+            Console.WriteLine("Y");
+        }
+        else
+        {
+            Console.WriteLine("N");
+        }
+
+        DuplicateKeys(Sarray, ref output);
+
+        //Output of repeated elements
+        foreach (string n in output)
+        {
+            Console.WriteLine(n);
+        }
+        
+        Console.ReadLine();
+    }
+
+    public static void DuplicateKeys(string[] array, ref string[] output)
+    {
+        //Record of repeated elements to output array
+        var duplKeys = array.GroupBy(x => x)
+                        .Where(group => group.Count() > 1)
+                        .Select(group => group.Key);
+
+        for (int i = 0; i < duplKeys.Count(); i++)
+        {
+            output[i] = duplKeys.ElementAt(i);
+        }
+           
+            
+    }
+    public static bool IFDuplicateKeys(string[] array)
+    {
+        //Detection if array contain repeates elements
+        var duplKeys = array.GroupBy(x => x)
+                        .Where(group => group.Count() > 1)
+                        .Select(group => group.Key);
+        if (duplKeys.Count() != 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
---
+++ Inteface_DefaultImpementation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ConsoleApp_NetCore
+{
+    class Program1
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Inteface_DefaultImpementation
+    {
+        static void Main(string[] args)
+        {
+            IMovable tom = new Person();
+            Car tesla = new Car();
+            tom.Move();     // Walking
+            tesla.Move();   // Driving
+            Console.ReadLine();
+        }
+    }
+
+    interface IMovable
+    {
+        void Move()
+        {
+            Console.WriteLine("Walking");
+        }
+    }
+
+    class Person : IMovable
+    {
+       
+    }
+
+     
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Driving");
+        }
+    }
+
+}
