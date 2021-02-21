895ba7112312d49f44c1f5bcaa7e67b8e236e06b
Danil <58917020+Leonardo2526@users.noreply.github.com>
2021-02-21 19:49:12 +0300

    Add files via upload

---
+++ CommonComands/CodeComments.cs
@@ -0,0 +1,25 @@
+﻿namespace CodeComments
+{
+    class Class2
+    {
+        static void Main (string[] args)
+        {
+            //Code comments
+
+            /* Add here as many lines 
+             of comments
+            as you want
+            */
+
+            //TODO: Code comments task list
+            //HACK: Code comments task list
+            //NOTE: Code comments task list
+            //UNDONE: Code comments task list
+
+
+            //To turn it on: View>Task List 
+
+        }
+
+    }
+}
---
+++ CommonComands/VariableScope.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableScope
+{
+    class Program
+     {
+        private static string helloClass = "Hello, class";
+
+        static void Main(string[] args)
+        {
+            string helloLocal = "Hello, local!";
+            Console.WriteLine(helloLocal);
+            Console.WriteLine(helloClass);
+            DoStuff();
+        }
+
+        static void DoStuff()
+        {
+            Console.WriteLine("A message from DoStuff: " + helloClass);
+            Console.ReadLine();
+        }
+        }
+}
